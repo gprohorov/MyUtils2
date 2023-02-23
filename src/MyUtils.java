6d961b07176334219f9a8c62152ee7f023cc37ba
gprohorov <george.prokhorov@external.getneteurope.com>
2023-02-23 22:49:57 +0200

    clean code

--- src/MyUtils.java
+++ src/MyUtils.java
@@ -66,6 +66,51 @@ class Circle extends Shape{
     }
 }
 
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(String name, double width, double height) {
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
+
 class Student {
     private int id;
     private String name;
@@ -117,50 +162,7 @@ class Student {
     }
 }
 
-class Rectangle extends Shape {
-    private double width;
-    private double height;
 
-    public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    public Rectangle(String name, double width, double height) {
-        super(name);
-        this.width = width;
-        this.height = height;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    @Override
-    public double getArea() {
-        return width * height;
-    }
-
-    @Override
-    public String toString() {
-        return "Rectangle{" +
-                "width=" + width +
-                ", height=" + height +
-                '}';
-    }
-}
 
 class Employee {
     private String name;
